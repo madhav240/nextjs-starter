b2c18841c09f8f58e43d04f7abe43a5b620327bd
madhav240 <madhavmishra1124@gmail.com>
2024-09-25 15:41:03 +0530

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,56 @@
+const { resolve } = require('node:path');
+
+const project = resolve(__dirname, 'tsconfig.json');
+
+module.exports = {
+  root: true,
+  extends: [
+    ...[
+      '@vercel/style-guide/eslint/node',
+      '@vercel/style-guide/eslint/browser',
+      '@vercel/style-guide/eslint/typescript',
+      '@vercel/style-guide/eslint/react',
+      '@vercel/style-guide/eslint/next',
+    ].map(require.resolve),
+    'prettier',
+    'plugin:tailwindcss/recommended',
+  ],
+  parserOptions: {
+    project,
+  },
+  globals: {
+    React: true,
+    JSX: true,
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: ['node_modules/', 'dist/', '.next/', '*static/'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'import/no-default-export': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'react/prop-types': 0,
+    'no-unused-vars': 0,
+    'react/no-unescaped-entities': 0,
+    'no-useless-escape': 'off',
+    'no-redeclare': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-misused-promises': 'off',
+    '@typescript-eslint/ban-types': 'warn',
+    'eslint-disable-next-line eslint-comments/require-description': 'off',
+    'tailwindcss/no-custom-classname': 'error',
+    'tailwindcss/no-contradicting-classname': 'error',
+  },
+  plugins: ['tailwindcss', 'prettier', 'react-hooks', 'react-refresh'],
+};
